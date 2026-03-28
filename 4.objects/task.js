0637888cb06fd658d9929d3a286c4c57925ebd19
Vlad <vladshoshin19@gmail.com>
2026-03-28 22:22:08 +0700

    Четвёртое задание исправил ошибки

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,35 +1,32 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (!this.marks) {
-    console.log("Ошибка: Студент отчислен");
-    return;
-  }
-
-  this.marks.push(...marks);
+    if (student1.hasOwnProperty('marks')) {
+      student1.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  if (!this.marks || this.marks.length === 0) {
-    return 0;
-  }
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
 
-  const sum = this.marks.reduce((acc, curr) => acc + curr, 0);
-  return sum / this.marks.length;
+    const sum = this.marks.reduce((acc, curr) => acc + curr, 0);
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
+    delete this.subject;
+    delete this.marks;
 
-  this.excluded = reason;
+    this.excluded = reason;
 }
